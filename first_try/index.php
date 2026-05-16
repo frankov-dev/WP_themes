63e0853417be1dbe29062cbd74a071fe54fb544c
Dmytro <dmytro.frankov@gmail.com>
2026-05-16 18:57:45 +0300

    refactor theme structure: update .gitignore, add header and footer files, and modify index.php

--- first_try/index.php
+++ first_try/index.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>First Try</title>
-    <?php wp_head(); ?>
-</head>
-<body>
-    <h1>Успіх! Тема працює.</h1>
-    <p>Це повідомлення виводиться з файлу index.php у папці first_try.</p>
-    <?php wp_footer(); ?>
-</body>
-</html>
+<?php get_header(); // Ця функція каже WP: "Візьми код із header.php" ?>
+
+<main style="padding: 20px;">
+    <h2>Це основний контент сторінки</h2>
+    <p>Тут ми будемо виводити наші записи або блоки ACF.</p>
+</main>
+
+<?php get_footer(); // Ця функція каже WP: "Візьми код із footer.php" ?>
