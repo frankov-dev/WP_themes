5b33f6c9b736c4d7fc10c6780028511a92e9896d
Dmytro <dmytro.frankov@gmail.com>
2026-05-16 18:40:38 +0300

    Initial commit

---
+++ first_try/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>First Try</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <h1>Успіх! Тема працює.</h1>
+    <p>Це повідомлення виводиться з файлу index.php у папці first_try.</p>
+    <?php wp_footer(); ?>
+</body>
+</html>
