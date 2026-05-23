a1fa6f999be98954d113aad89604c9424570e230
Dmytro <dmytro.frankov@gmail.com>
2026-05-23 14:12:01 +0300

    uncomment script enqueue for main.js in functions.php

--- Estate Pro/functions.php
+++ Estate Pro/functions.php
@@ -9,10 +9,9 @@
 function estate_pro_enqueue_assets() {
     wp_enqueue_style( 'estate-pro-style', get_stylesheet_uri(), array(), '1.0' );
     // При потребі підключіть скрипти так:
-    // wp_enqueue_script( 'estate-pro-main', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0', true );
+    wp_enqueue_script( 'estate-pro-main', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'estate_pro_enqueue_assets' );
-
 
 function estate_pro_theme_setup() {
     
