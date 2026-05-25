dc0622778d6be6b7b78f26c034b5285b624c98fe
Dmytro <dmytro.frankov@gmail.com>
2026-05-25 18:30:49 +0300

    refactor: enhance theme setup by adding primary menu registration and clean up commented block registration code

--- TechReview/functions.php
+++ TechReview/functions.php
@@ -3,12 +3,19 @@
 function techreview_enqueue_styles() {
     wp_enqueue_style('techreview-style', get_stylesheet_uri());
 }
-
 add_action('wp_enqueue_scripts', 'techreview_enqueue_styles');
 
 function techreview_setup() {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    register_nav_menus(array(
+        'primary' => 'Primary Menu',
+    ));
 }
-
 add_action('after_setup_theme', 'techreview_setup');
+
+// function techreview_acf_register_blocks() {
+//     if (function_exists('acf_register_block_type')) {
+
+//     }
+// }
