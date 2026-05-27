e5359e1f0b4a0d911a5d8e597e3eb980de16964f
Dmytro <dmytro.frankov@gmail.com>
2026-05-27 20:30:35 +0300

    move ACF block registration to a separate file for better organization

---
+++ TechReview/inc/acf-blocks.php
@@ -0,0 +1,30 @@
+<?php
+function techreview_acf_register_blocks() {
+    if ( function_exists('acf_register_block_type') ) {
+        
+        // Блок Плюсів та Мінусів
+        acf_register_block_type( array(
+            'name'            => 'pros_cons',
+            'title'           => 'Плюси та Мінуси',
+            'description'     => 'Блок для порівняння переваг та недоліків гаджета.',
+            'render_template' => 'template-parts/blocks/pros-cons.php',
+            'category'        => 'formatting',
+            'icon'            => 'thumbs-up',
+        ) );
+
+        acf_register_block_type( array(
+            'name'            => 'specs_table',
+            'title'           => 'Таблиця Характеристик',
+            'description'     => 'Блок для відображення технічних характеристик у вигляді таблиці.',
+            'render_template' => 'template-parts/blocks/specs-table.php',
+            'category'        => 'formatting',
+            'icon'            => 'table-row-after',
+        ));
+
+        
+    }
+}
+
+
+
+add_action('acf/init', 'techreview_acf_register_blocks');
