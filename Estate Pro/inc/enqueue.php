0a05cea762284ddca9831e374b1895de0012b162
Dmytro <dmytro.frankov@gmail.com>
2026-06-16 10:59:18 +0300

    feat: update styles and enqueue fonts for improved theme aesthetics

--- Estate Pro/inc/enqueue.php
+++ Estate Pro/inc/enqueue.php
@@ -1,7 +1,8 @@
 <?php
 
 function estate_pro_enqueue_assets() {
-    wp_enqueue_style( 'estate-pro-style', get_stylesheet_uri(), array(), '1.0' );
+    wp_enqueue_style( 'estate-pro-fonts', 'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap', array(), null );
+    wp_enqueue_style( 'estate-pro-style', get_stylesheet_uri(), array( 'estate-pro-fonts' ), '1.0' );
     wp_enqueue_script( 'estate-pro-main', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'estate_pro_enqueue_assets' );
